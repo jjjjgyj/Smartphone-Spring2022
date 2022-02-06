265aeb07fa45381bdbb87a3cae98448bc6368b4b
JJJJ <yunjiagao27@gmail.com>
2022-02-06 11:11:25 -0800

    check in for Seque Example

---
+++ SegueExample/SegueExample/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SegueExample
+//
+//  Created by Yunjia Gao on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var txtFirstName: UITextField!
+    
+    @IBOutlet weak var txtLastName: UITextField!
+    
+    @IBAction func goToSecondVC(_ sender: Any) {
+        performSegue(withIdentifier: "sequeToSecondVC", sender: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "sequeToSecondVC" {
+            let secondVC = segue.destination as! SecondViewController
+            secondVC.welcomeStr = "Welcome \(txtFirstName.text!), \(txtLastName.text!)"
+            
+            
+            
+        }
+    }
+
+}
+
